61ea91bbe5f05b2d67b680db4e6790acb9388211
Benjamin Arbogast <benjamin.arbogast@gmail.com>
2017-07-26 00:08:38 +0200

    Restore state from url

--- src/configurator/index.js
+++ src/configurator/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {extendObservable, action} from 'mobx'
+import {extendObservable, action, toJS} from 'mobx'
 import { Tab } from 'semantic-ui-react'
 import {observer} from 'mobx-react'
 
@@ -9,22 +9,37 @@ import Chart from './Chart'
 
 
 class Store {
-  constructor(){
-    extendObservable(this, {
-      dataSet: new DataSet(),
-      setChartType: action(v => this.chartType = v),
-      chartType: 'bar',
-      get isReadyToRender(){
-        return store.chartType && this.dataSet.groupDimension && store.dataSet.dataDimension
-      },
-    })
+  constructor(data){
+    if(data){
+
+    } else {
+      data = {
+        setChartType: action(v => this.chartType = v),
+        chartType: 'bar',
+        get isReadyToRender(){
+          return store.chartType && this.dataSet.groupDimension && store.dataSet.dataDimension
+        },
+        get url(){
+          const obj = this.toJS()
+          console.log(JSON.stringify(obj))
+        },
+
+        get jsonString(){
+          const obj = toJS(this)
+          delete obj.dataSet
+          return JSON.stringify(obj)
+        }
+      }
+    }
+
+    data.dataSet = new DataSet()
+    extendObservable(this, data)
   }
 }
 
 
 const Configurator = observer(({store}) => {
   return <div>
-
     <label>JSON-stat URL
       <input type="text" value={store.dataSet.jsonstatUrl} onChange={(e) => store.dataSet.setjsonStatUrl(e.target.value)}/>
     </label>
@@ -43,6 +58,10 @@ const Configurator = observer(({store}) => {
       </select>
     </label>}
 
+    <br />
+
+    <a href={'?json=' + encodeURIComponent(store.jsonString)}>Link</a>
+
     <Tab panes={[
       { menuItem: 'Data', render: () => <Tab.Pane><DataTable store={store.dataSet}/></Tab.Pane> },
       { menuItem: 'Chart', render: () => <Tab.Pane><Chart store={store} /></Tab.Pane> },
@@ -52,6 +71,21 @@ const Configurator = observer(({store}) => {
 })
 
 
+// https://stackoverflow.com/a/901144/4287172
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
 
-const store = new Store();
+
+let data
+if(getParameterByName('json')){
+  data = JSON.parse(getParameterByName('json'))
+}
+const store = new Store(data);
 export default () => <Configurator store={store} />
